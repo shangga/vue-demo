1bb7b1e7afcae1767c910313699d535cb4329e6f
shangga <18204611912@163.com>
2017-09-26 00:35:01 +0800

    genxindaima

--- src/views/tabContent.vue
+++ src/views/tabContent.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <Tabs>
-        <TabPane label="标签一" name="name1">标签一的内容</TabPane>
-        <TabPane label="标签二" name="name2">标签二的内容</TabPane>
-        <TabPane label="标签三" name="name3">标签三的内容</TabPane>
+        <TabPane label="标签一" name="name1"></TabPane>
+        <TabPane label="标签二" name="name2"></TabPane>
+        <TabPane label="标签三" name="name3"></TabPane>
     </Tabs>
     <router-view></router-view>
   </div>
 </template>
 <script>
+// import axios from 'axios'
 // import pageOne from './pageOne.vue'
 export default {
   data () {
@@ -20,6 +21,17 @@ export default {
     goDetail (str) {
       this.$router.push({name: str})
     }
+  },
+  mounted () {
+    // axios.post('', {
+
+    // })
+    // .then((response) => {
+    //   console.log(response)
+    // })
+    // .catch((error) => {
+    //   console.log(error)
+    // })
   }
 }
 </script>
